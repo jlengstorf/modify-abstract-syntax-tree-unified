61474e37f7d6f2eab36dd994077218bf9804d45a
Jason Lengstorf <jason@lengstorf.com>
2019-11-24 15:48:12 -0800

    feat: convert Markdown to HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// TODO write some code
+const fs = require('fs');
+const unified = require('unified');
+const markdown = require('remark-parse');
+const html = require('remark-html');
+
+const contents = unified()
+  .use(markdown)
+  .use(html)
+  .processSync(fs.readFileSync(`${process.cwd()}/content/home.md`))
+  .toString();
+
+console.log(contents);
