4485e25c1375fc2c28fdb85a4614afce2da6dbb7
Jason Lengstorf <jason@lengstorf.com>
2019-11-24 17:22:31 -0800

    feat: convert remark to rehype

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const unified = require('unified');
 const markdown = require('remark-parse');
-const html = require('remark-html');
+const remark2rehype = require('remark-rehype');
+const html = require('rehype-stringify');
 
 const contents = unified()
   .use(markdown)
+  .use(remark2rehype)
   .use(() => tree => console.log(JSON.stringify(tree, null, 2)))
   .use(html)
   .processSync(fs.readFileSync(`${process.cwd()}/content/home.md`))
