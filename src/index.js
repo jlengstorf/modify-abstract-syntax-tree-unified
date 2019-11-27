033929597fdade25c17978076e12ad67cdc0f961
Jason Lengstorf <jason@lengstorf.com>
2019-11-26 17:45:41 -0800

    feat: save output to file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const unified = require('unified');
 const markdown = require('remark-parse');
 const remark2rehype = require('remark-rehype');
+const doc = require('rehype-document');
 const html = require('rehype-stringify');
 
 const imgToFigure = require('./img-to-figure');
@@ -10,8 +11,15 @@ const contents = unified()
   .use(markdown)
   .use(remark2rehype)
   .use(imgToFigure)
+  .use(doc, { title: 'A Transformed Document!' })
   .use(html)
   .processSync(fs.readFileSync(`${process.cwd()}/content/home.md`))
   .toString();
 
-console.log(contents);
+const outputDir = `${process.cwd()}/public`;
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
+
+fs.writeFileSync(`${outputDir}/home.html`, contents);
